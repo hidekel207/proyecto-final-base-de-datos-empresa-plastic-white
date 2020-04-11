20a000c659c90388c5f25ea1b4149a204c07b20a
hidekel207 <63486333+hidekel207@users.noreply.github.com>
2020-04-11 00:38:53 -0400

    subida inicial del proyecto plastic white

---
+++ base de datos plastic white - copia.sql
@@ -0,0 +1,20 @@
+--mostrar todos los datos de la tabla plastic white--
+select *from productos
+
+-- mostrar solo la columna departamento --
+select departamento from productos
+
+--mostra solo la columna cantidad por paquete del producto--
+select [cantidad por paquete] from productos
+
+--mostra solo la columna cantidad de almacenamiento --
+select [catiadad de almacenamiento]  from productos
+
+--mostrar solo la columna cantidad por paquete del producto --
+select
+
+-- seleccionar las columnas departamento y nombre del producto  --
+select [nombre del producto], [departamento]  from productos
+select * from productos
+
+
